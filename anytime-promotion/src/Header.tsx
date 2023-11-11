172b96020a468688b0aa4833169e0e30d69caea3
Devan Rivera <devanrivera98@gmail.com>
2023-11-11 01:13:45 +0000

    conditionally rendering logo

--- anytime-promotion/src/Header.tsx
+++ anytime-promotion/src/Header.tsx
@@ -2,28 +2,37 @@ import {Outlet} from 'react-router-dom'
 import { useState, useEffect } from 'react'
 
 export default function Header() {
-  const [isOpen, setIsOpen] = useState(false)
+  // const [isOpen, setIsOpen] = useState(false)
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768)
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   function handleResize() {
     setWindowWidth(window.innerWidth)
-    console.log(windowWidth)
   }
 
   useEffect(() => {
     window.addEventListener('resize', handleResize)
 
-    return (
+    return () => {
       window.removeEventListener('resize', handleResize)
-    )
+    }
   }, [])
+
+  useEffect(() => {
+    if (windowWidth < 768) {
+      setIsMobile(true)
+  } else {
+    setIsMobile(false)
+  }
+  }, [windowWidth])
 
 return (
   <>
   <header className='bg-black text-white py-4'>
     <div className='container flex justify-between mx-auto'>
-      <h4 className='ml-5'>Anytime Promotion</h4>
-        <img className='header-white-logo' src='anytime-white-logo.png' />
+      {/* <h4 className='ml-5'>Anytime Promotion</h4> */}
+      <h4 className={`ml-5 ${isMobile ? 'hidden' : '' }`}>Anytime Promotion</h4>
+        <img className={`header-white-logo ${isMobile ? '' : 'hidden'}`} src='anytime-white-logo.png' />
       <nav>
         <ul>
           <li className='inline-block'>
