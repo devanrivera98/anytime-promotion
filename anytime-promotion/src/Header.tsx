9a66b3e8293cd26f7a901cb55a65ec05918b9432
Devan Rivera <devanrivera98@gmail.com>
2023-11-11 00:33:54 +0000

    working on header

--- anytime-promotion/src/Header.tsx
+++ anytime-promotion/src/Header.tsx
@@ -1,30 +1,48 @@
 import {Outlet} from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false)
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  function handleResize() {
+    setWindowWidth(window.innerWidth)
+    console.log(windowWidth)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+
+    return (
+      window.removeEventListener('resize', handleResize)
+    )
+  }, [])
+
 return (
   <>
-  <header className='bg-black text-white'>
-    <div className='container flex justify-between align-items'>
-      <h4>Anytime Promotion</h4>
+  <header className='bg-black text-white py-4'>
+    <div className='container flex justify-between mx-auto'>
+      <h4 className='ml-5'>Anytime Promotion</h4>
+        <img className='header-white-logo' src='anytime-white-logo.png' />
       <nav>
         <ul>
-          <li>
+          <li className='inline-block'>
             <a>Home</a>
           </li>
-          <li>
+          <li className='inline-block'>
             <a>Events</a>
           </li>
-          <li>
+          <li className='inline-block'>
             <a>Autograph Validator</a>
           </li>
-          <li>
+          <li className='inline-block'>
             <a>Contact Us</a>
           </li>
         </ul>
       </nav>
     </div>
 
-    {/* <img className='header-white-logo' src='anytime-white-logo.png' /> */}
+
   </header>
   <Outlet/>
   </>
