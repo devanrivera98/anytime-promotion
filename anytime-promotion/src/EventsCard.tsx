ae007e3f90b0903ee67f76883e161074d22cc999
Devan Rivera <devanrivera98@gmail.com>
2023-11-15 23:55:27 +0000

    made an event card

--- anytime-promotion/src/EventsCard.tsx
+++ anytime-promotion/src/EventsCard.tsx
@@ -1,21 +1,29 @@
-export default function EventsCard() {
+interface EventsCardProps {
+  guest: string;
+  date: string;
+  image: string;
+  text: string;
+  color: string;
+}
+
+export default function EventsCard({guest,date,image,text, color} : EventsCardProps) {
 
   return (
 
     <>
-      <div className="bg-darkRed border border-4 border-black rounded rounded-lg m-5 md:mx-20 lg:w-1/3">
+      <div className={`${color} border border-4 border-black rounded rounded-lg m-5 md:mx-20 lg:w-1/3`}>
         <div className="text-xl text-gray-300 flex justify-center">
-          <h1 className="font-black">Guest Don Walker</h1>
+          <h1 className="font-black">{guest}</h1>
         </div>
         <div className="text-white flex justify-center">
-          <h1>Posted November 18,2023</h1>
+          <h1>{date}</h1>
         </div>
         <div className="flex justify-center">
-          <img className="w-64" src="/anytime-promotion-test.jpeg" />
+          <img className="w-64" src={image} />
         </div>
         <div className="p-1 mx-5">
           <p className="text-offWhite leading-7">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui nunc mattis enim ut tellus elementum. Sit amet risus nullam eget. Eget sit amet tellus cras adipiscing. Felis imperdiet proin fermentum leo vel orci porta non pulvinar. Risus commodo viverra maecenas accumsan lacus vel facilisis. Risus commodo viverra maecenas accumsan lacus vel facilisis volutpat.
+            {text}
           </p>
         </div>
       </div>
