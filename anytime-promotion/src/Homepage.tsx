43fa5305a922a0937793ae1a64e5c8b4058d2c1e
Devan Rivera <devanrivera98@gmail.com>
2023-11-16 04:00:38 +0000

    added icons to homepage

--- anytime-promotion/src/Homepage.tsx
+++ anytime-promotion/src/Homepage.tsx
@@ -1,6 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faAngleLeft} from '@fortawesome/free-solid-svg-icons';
 import {faAngleRight} from '@fortawesome/free-solid-svg-icons';
+import { faCircle as solidCircle } from '@fortawesome/free-solid-svg-icons';
+import { faCircle as regularCircle } from '@fortawesome/free-regular-svg-icons';
 
 export default function Homepage() {
 
@@ -9,8 +11,22 @@ export default function Homepage() {
       <div className="pt-5 bg-offWhite homepage">
         <div className="flex justify-center">
           <FontAwesomeIcon className='md:block hidden my-auto icon-arrow' icon={faAngleLeft} size="2xl" style={{color: "#444040",}} />
-          <img className="h-64" src="https://t4.ftcdn.net/jpg/04/92/06/53/360_F_492065380_eSlHdkdNBy6ArNkk5gV0Lme1qhvTosSI.jpg" />
+          <img className="h-64 rounded rounded-xl banner" src="https://t4.ftcdn.net/jpg/04/92/06/53/360_F_492065380_eSlHdkdNBy6ArNkk5gV0Lme1qhvTosSI.jpg" />
           <FontAwesomeIcon className='md:block hidden my-auto icon-arrow' icon={faAngleRight} size="2xl" style={{color: "#444040",}} />
+        </div>
+        <div className='flex justify-center pt-2 space-x-8'>
+            <div>
+               <FontAwesomeIcon size='lg' icon={solidCircle} style={{ color: '#000000' }} />
+            </div>
+            <div>
+                 <FontAwesomeIcon size='lg'  icon={regularCircle} style={{ color: '#000000' }} />
+            </div>
+            <div>
+               <FontAwesomeIcon size='lg'  icon={regularCircle} style={{ color: '#000000' }} />
+            </div>
+            <div>
+                 <FontAwesomeIcon size='lg'  icon={regularCircle} style={{ color: '#000000' }} />
+            </div>
         </div>
       </div>
     </>
