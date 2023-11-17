0015d0cd056835369e7dcb5306f106bafce0faae
Devan Rivera <devanrivera98@gmail.com>
2023-11-17 22:03:29 +0000

    marquee placed on homepage

--- anytime-promotion/src/Homepage.tsx
+++ anytime-promotion/src/Homepage.tsx
@@ -1,19 +1,17 @@
-
 import Slider from './Slider';
+import Marquee from "react-fast-marquee";
 
 export default function Homepage() {
-
   return (
     <>
       <Slider />
-      <div className='relative flex overflow-x-hidden bg-darkBlue text-white'>
-        <div className='animate-marquee whitespace-no-wrap'>
-          <h1 className=''>Signed with artist such as Joe Rubinstein, Larry Houston, Chuck Patton, Jim Krueger, Javier Hernandez, Don Walker, and many more!  </h1>
+      <Marquee className='bg-darkBlue my-4 py-4' pauseOnHover={true}>
+        <div>
+          <h1 className='md:text-2xl text-white'>
+            &nbsp;Signed with artists such as Joe Rubinstein, Larry Houston, Chuck Patton, Jim Krueger, Javier Hernandez, Don Walker, and many more!
+          </h1>
         </div>
-        <div className='absolute top-0 py-12 animate-marquee2 whitespace-no-wrap'>
-          <h1>Signed with artists such as Joe Rubinstein, Larry Houston, Chuck Patton, Jim Krueger, Javier Hernandez, Don Walker, and many more!  </h1>
-        </div>
-      </div>
+      </Marquee>
     </>
-  )
+  );
 }
--- anytime-promotion/tailwind.config.js
+++ anytime-promotion/tailwind.config.js
@@ -19,20 +19,6 @@ export default {
         offWhite: '#F5F5F5',
         darkBlue: '#243551'
       },
-      animation: {
-        marquee: 'marquee 5s linear infinite',
-        marquee2: 'marquee2 5s linear infinite'
-      },
-      keyframes: {
-        marquee: {
-          '0%': {transform: 'translateX(10%)'},
-          '100%': {transform: 'translateX(-100%)'},
-        },
-        marquee2: {
-          '0%': {transform: 'translateX(100%)'},
-          '100%': {transform: 'translateX(0%)'},
-        },
-      },
     },
   },
   plugins: [],
