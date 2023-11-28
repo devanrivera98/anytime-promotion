8f8982ff1ad6acc83118be9463967ff98df0825f
Devan Rivera <devanrivera98@gmail.com>
2023-11-28 22:57:34 +0000

    created a smooth icon for mobile app

--- anytime-promotion/src/Header.tsx
+++ anytime-promotion/src/Header.tsx
@@ -1,6 +1,7 @@
 import {Outlet} from 'react-router-dom'
 import { useState } from 'react'
 import Banner from './Banner';
+// import { faToggleOff } from '@fortawesome/free-solid-svg-icons';
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false)
@@ -18,12 +19,17 @@ return (
           <div className='md:block hover:animate-pulse hidden'>Anytime Promotion</div>
           <img className='w-12 h-15 md:hidden' src='/anytime-white-logo.jpg'></img>
         </a>
-        <button onClick={handleIconClick} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none hover:ring-2 hover:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+        <a href='#' id='menu-icon' className={isOpen ? 'close' : ''} onClick={handleIconClick}>
+          <div className='bar'></div>
+          <div className='bar'></div>
+          <div className='bar'></div>
+        </a>
+        {/* <button onClick={handleIconClick} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none hover:ring-2 hover:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
           <span className="sr-only">Open main menu</span>
           <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
               <path stroke="currentColor" d="M1 1h15M1 7h15M1 13h15"/>
           </svg>
-        </button>
+        </button> */}
         {/* needs hidden */}
         <div className={`w-full md:block md:w-auto p-2 md:p-0 ${isOpen ? '' : 'hidden'}`}>
           <ul className='flex flex-col p-4 md:p-0 border border-gray-400 rounded-md bg-gray-900 md:flex-row md:mt-0 md:border-0 md:bg-black '>
