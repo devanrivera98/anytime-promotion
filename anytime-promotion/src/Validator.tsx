da31bc52b805540f09d2f0ebfaddcec303d0a0dd
Devan Rivera <devanrivera98@gmail.com>
2023-11-28 03:45:41 +0000

    added visual changes to validation page

--- anytime-promotion/src/Validator.tsx
+++ anytime-promotion/src/Validator.tsx
@@ -1,12 +1,37 @@
+import CaspioIntegration from "./CaspioIntegration";
+
 export default function Validator() {
   return (
     <>
     <div>
       <div className="flex justify-center pt-5">
-        <h1 className="text-2xl bg-darkBlue rounded p-1 text-white">Authograph Verification Check</h1>
+        <h1 className="text-2xl md:text-4xl p-1 my-5">Authentication</h1>
       </div>
-      <div>
-        
+      <div className="leading-6 sm:w-1/2 mx-auto">
+        <div className="mx-5">
+          <h1 className="text-gray-900 font-extrabold text-xl">Authentication Check:</h1>
+        </div>
+        <div className="px-5 py-3" >
+          <p>Verify the authenticity of your Anytime Promotion signed item by using our Autograph Verification. The serial number included in your COA will provide you with: </p>
+            {/* <br /> &bull;  Issue Signed <br /> &bull;  Who signed <br /> &bull; Date of signing <br />  Check provided by Caspio. <br /> Enter your item's serial number below: </p> */}
+          <div className="leading-9">
+            <div>
+              &bull;  Issue Signed
+            </div>
+            <div>
+              &bull;  Who Signed
+            </div>
+            <div>
+              &bull;  Date of Signing
+            </div>
+            <div>
+              Enter your item's serial number below:
+            </div>
+          </div>
+        </div>
+        <div>
+          <CaspioIntegration/>
+        </div>
       </div>
     </div>
     </>
