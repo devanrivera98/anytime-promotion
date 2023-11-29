fae000c8b94b94f2950fd86dea791ef0a4ec3ae7
Devan Rivera <devanrivera98@gmail.com>
2023-11-29 19:52:52 +0000

    made visual changes to navbar

--- anytime-promotion/src/Header.tsx
+++ anytime-promotion/src/Header.tsx
@@ -1,5 +1,5 @@
 import {Outlet} from 'react-router-dom'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Banner from './Banner';
 // import { faToggleOff } from '@fortawesome/free-solid-svg-icons';
 
@@ -10,9 +10,25 @@ export default function Header() {
     setIsOpen(!isOpen)
   }
 
+  const handleResize = () => {
+    if (window.innerWidth > 768) {
+      setIsOpen(false)
+    }
+  }
+
+  useEffect(() => {
+    handleResize();
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
 return (
   <>
-  <header>
+  <header className={`mobile-open ${isOpen ? 'fixed inset-0 z-20' : ''}`}>
     <nav className='bg-black text-white text-lg py-4'>
       <div className='flex flex-wrap items-center justify-between mx-auto p-4'>
         <a href='/' className='pl-3 hover:cursor-pointer'>
@@ -24,19 +40,12 @@ return (
           <div className='bar'></div>
           <div className='bar'></div>
         </a>
-        {/* <button onClick={handleIconClick} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none hover:ring-2 hover:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-          <span className="sr-only">Open main menu</span>
-          <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-              <path stroke="currentColor" d="M1 1h15M1 7h15M1 13h15"/>
-          </svg>
-        </button> */}
-        {/* needs hidden */}
-        <div className={`w-full md:block md:w-auto p-2 md:p-0 ${isOpen ? '' : 'hidden'}`}>
-          <ul className='flex flex-col p-4 md:p-0 border border-gray-400 rounded-md bg-gray-900 md:flex-row md:mt-0 md:border-0 md:bg-black '>
+        <div className={`w-full h-screen pt-5 md:h-auto md:block md:w-auto p-2 md:p-0 ${isOpen ? '' : 'hidden'}`}>
+          <ul className='flex flex-col justify-evenly h-5/6 items-center p-4 md:p-0 border border-gray-400 rounded-lg md:flex-row md:mt-0 md:border-0 md:bg-black '>
             <li>
               <a href='/' className='block py-2 px-3 text-white rounded md:hover:bg-transparent hover:bg-gray-800 md:p-0 md:bg-black md:hover:underline underline-offset-4'>Home</a>
             </li>
-            <li>
+            <li >
               <a href='/events' className='block py-2 px-3 text-white rounded md:hover:bg-transparent hover:bg-gray-800 md:p-0 md:bg-black md:hover:underline underline-offset-4'>Events</a>
             </li>
             <li>
