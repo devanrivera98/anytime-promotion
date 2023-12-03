2365dd0c5e9cdde72e45746ff0ee542d9056a258
Devan Rivera <devanrivera98@gmail.com>
2023-12-03 23:19:17 +0000

    owner requested changes

--- anytime-promotion/src/Homepage.tsx
+++ anytime-promotion/src/Homepage.tsx
@@ -4,14 +4,14 @@ import Marquee from "react-fast-marquee";
 export default function Homepage() {
   return (
     <>
-      <Slider />
-      <Marquee className='bg-darkBlue my-4 py-4 z-10' pauseOnHover={true}>
+      <Marquee className='bg-darkRed mb-4 py-4 z-10' pauseOnHover={true}>
         <div>
           <h1 className='md:text-2xl text-white'>
             &nbsp;Signed with artists such as Joe Rubinstein, Larry Houston, Chuck Patton, Jim Krueger, Javier Hernandez, Don Walker, and many more!
           </h1>
         </div>
       </Marquee>
+      <Slider />
     </>
   );
 }
