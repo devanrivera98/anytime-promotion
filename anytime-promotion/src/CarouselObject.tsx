1ed4d8585fd44482181d21e6cf0c94d7f53f39b3
Devan Rivera <devanrivera98@gmail.com>
2024-02-02 00:54:02 +0000

    implemented hero banner images

--- anytime-promotion/src/CarouselObject.tsx
+++ anytime-promotion/src/CarouselObject.tsx
@@ -1,3 +1,1 @@
-export const sliderPhotos = [{number: '0', name: 'black banner', url: 'website-banner.png'}, {number: '1', name: 'blue banner', url: 'anytime-promotion-banner.png'}]
-
-// export const sliderPhotos = [{number: '0', name: 'black banner', url: 'website-banner.png'}, {number: '1', name: 'blue banner', url: ''}, {number: '3', name: 'red banner', url: 'https://t3.ftcdn.net/jpg/04/82/00/46/360_F_482004683_BWj2rOJrK9N6KAyvjEhTXpz0MRn0Lfa9.jpg'}, {number: '4', name: 'yellow banner', url: 'https://img.freepik.com/free-vector/bright-yellow-banner-with-halftone-effect_1017-36272.jpg'}]
+export const sliderPhotos = [{number: '0', name: 'black banner', url: 'whittier-comic-fest-banner.png', link: 'https://www.instagram.com/whittiercomicfest/'}, {number: '1', name: 'blue banner', url: 'anytime-promotion-banner.png', link: 'https://www.instagram.com/anytimepromotions/'}]
--- anytime-promotion/src/Slider.tsx
+++ anytime-promotion/src/Slider.tsx
@@ -16,7 +16,7 @@ export default function Slider() {
       function timerCallback() {
     setIsCurrent((isCurrent + 1 + sliderPhotos.length) % sliderPhotos.length)
   }
-  const intervalID = setInterval(timerCallback, 3000)
+  const intervalID = setInterval(timerCallback, 4000)
 
   //cleaning the interval
   return () => clearInterval(intervalID)
@@ -29,8 +29,10 @@ export default function Slider() {
     <div className="pt-5">
       <div className="flex justify-center items-center">
         <LeftArrow onCustomClick={() => setIsCurrent((isCurrent - 1 + sliderPhotos.length) % sliderPhotos.length)}/>
-        <div>
-          <img className="h-64 rounded rounded-xl banner select-none hover:cursor-pointer" src={sliderPhotos[isCurrent].url} alt={sliderPhotos[isCurrent].name} />
+        <div className='md:w-3/5 h-56'>
+          <a target='_blank' href={sliderPhotos[isCurrent].link}>
+          <img className="object-fill w-full h-full sm:rounded sm:rounded-xl banner select-none hover:cursor-pointer" src={sliderPhotos[isCurrent].url} alt={sliderPhotos[isCurrent].name} />
+          </a>
         </div>
         <RightArrow onCustomClick={() => setIsCurrent((isCurrent + 1 + sliderPhotos.length) % sliderPhotos.length)} />
       </div>
