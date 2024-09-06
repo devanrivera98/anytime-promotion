922beb15b85d464f9c1ab5a76da0d6148a296d70
Devan Rivera <devanrivera98@gmail.com>
2024-09-06 23:02:44 +0000

    adjusted form layout

--- anytime-promotion/src/ContactForm.tsx
+++ anytime-promotion/src/ContactForm.tsx
@@ -17,13 +17,18 @@ export default function ContactForm() {
         <div className="md:w-1/3 py-5">
           <div className="mx-3">
             <form name="react-contact-form" method="post" className="flex flex-col">
+              <legend id="contact-form" className="sr-only">Contact Form</legend>
               <input type='hidden' name='form-name' value='contact' />
+              <label htmlFor="name" className="sr-only">Full Name</label>
               <input type='text' name="name" className="bg-offWhite my-3 md:mt-5 md:mb-0.5 py-2 pl-1" placeholder="Full Name" required />
               <hr className="bg-black rounded rounded-xl py-0.5"/>
+              <label htmlFor="email" className="sr-only">Email</label>
               <input type='text' name="email" className="bg-offWhite my-3 md:my-0.5 py-2 pl-1" placeholder="Email" required />
              <hr className="bg-black rounded rounded-xl py-0.5"/>
+              <label htmlFor="subject" className="sr-only">Subject</label>
               <input type='text' name="subject" className="bg-offWhite my-3 md:my-0.5 py-2 pl-1" placeholder="Subject" required />
              <hr className="bg-black rounded rounded-xl py-0.5"/>
+             <label htmlFor="message" className="sr-only">Message</label>
               <textarea name="message" rows={12} className="bg-offWhite my-3 md:my-0.5 py-2 pl-1" placeholder="Message" required />
              <hr className="bg-black rounded rounded-xl py-0.5"/>
               <button type="submit" className="w-28 p-2 bg-darkRed text-white hover:bg-red-900  rounded rounded-lg mt-5">Submit</button>
