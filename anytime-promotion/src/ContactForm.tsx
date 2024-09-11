822cae4329e4ecd0fc4db80012730c03a458e88d
Devan Rivera <devanrivera98@gmail.com>
2024-09-11 22:05:15 +0000

    form update submitted

--- anytime-promotion/src/ContactForm.tsx
+++ anytime-promotion/src/ContactForm.tsx
@@ -16,20 +16,20 @@ export default function ContactForm() {
         </div>
         <div className="md:w-1/3 py-5">
           <div className="mx-3">
-            <form name="react-contact-form" method="post" className="flex flex-col">
+            <form name="react-contact-form" method="post" role="form" className="flex flex-col">
               <legend id="contact-form" className="sr-only">Contact Form</legend>
               <input type='hidden' name='form-name' value='contact' />
               <label htmlFor="name" className="sr-only">Full Name</label>
-              <input type='text' name="name" className="bg-offWhite my-3 md:mt-5 md:mb-0.5 py-2 pl-1" placeholder="Full Name" required />
+              <input id="name" type='text' name="name" className="bg-offWhite my-3 md:mt-5 md:mb-0.5 py-2 pl-1" placeholder="Full Name" required />
               <hr className="bg-black rounded rounded-xl py-0.5"/>
-              <label htmlFor="email" className="sr-only">Email</label>
+              <label id="email" htmlFor="email" className="sr-only">Email</label>
               <input type='text' name="email" className="bg-offWhite my-3 md:my-0.5 py-2 pl-1" placeholder="Email" required />
              <hr className="bg-black rounded rounded-xl py-0.5"/>
               <label htmlFor="subject" className="sr-only">Subject</label>
-              <input type='text' name="subject" className="bg-offWhite my-3 md:my-0.5 py-2 pl-1" placeholder="Subject" required />
+              <input id="subject" type='text' name="subject" className="bg-offWhite my-3 md:my-0.5 py-2 pl-1" placeholder="Subject" required />
              <hr className="bg-black rounded rounded-xl py-0.5"/>
              <label htmlFor="message" className="sr-only">Message</label>
-              <textarea name="message" rows={12} className="bg-offWhite my-3 md:my-0.5 py-2 pl-1" placeholder="Message" required />
+              <textarea id="message" name="message" rows={12} className="bg-offWhite my-3 md:my-0.5 py-2 pl-1" placeholder="Message" required />
              <hr className="bg-black rounded rounded-xl py-0.5"/>
               <button type="submit" className="w-28 p-2 bg-darkRed text-white hover:bg-red-900  rounded rounded-lg mt-5">Submit</button>
             </form>
